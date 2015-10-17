ef329bf611c441512f320ebaa6d8d9da8019be97
Matteo Merli <mmerli@apache.org>
2015-10-16 17:36:09 -0700

    Recycle instances of WriteEntryProcessor and ReadEntryProcessor

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/proto/BookieRequestProcessor.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/proto/BookieRequestProcessor.java
@@ -190,7 +190,7 @@ public class BookieRequestProcessor implements RequestProcessor {
     }
 
     private void processAddRequest(final BookieProtocol.Request r, final Channel c) {
-        WriteEntryProcessor write = new WriteEntryProcessor(r, c, this);
+        WriteEntryProcessor write = WriteEntryProcessor.create(r, c, this);
         if (null == writeThreadPool) {
             write.run();
         } else {
@@ -199,7 +199,7 @@ public class BookieRequestProcessor implements RequestProcessor {
     }
 
     private void processReadRequest(final BookieProtocol.Request r, final Channel c) {
-        ReadEntryProcessor read = new ReadEntryProcessor(r, c, this);
+        ReadEntryProcessor read = ReadEntryProcessor.create(r, c, this);
         if (null == readThreadPool) {
             read.run();
         } else {
